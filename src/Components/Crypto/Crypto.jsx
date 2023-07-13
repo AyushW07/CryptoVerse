ccf2f92b5be8b48055b1aa12935c1b0b893296f9
aayush <ayushwasnik999@gmail.com>
2023-07-13 16:37:55 +0530

    added cryptos

--- src/Components/Crypto/Crypto.jsx
+++ src/Components/Crypto/Crypto.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+
+import { useGetCryptoQuery } from "../../services/cryptoAPI";
 
 function Crypto() {
+  const { data: cryptoList, isfetching } = useGetCryptoQuery();
+  const [crypto, setCrypto] = useState(cryptoList?.data?.coins);
+
+  console.log(crypto);
+
   return (
-    <div>Crypto</div>
-  )
+    <Fragment>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {crypto.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img src={currency.iconUrl} className="crypto-image" />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
 }
 
-export default Crypto
+export default Crypto;
--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -4,10 +4,12 @@ import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 
 import { useGetCryptoQuery } from "../../services/cryptoAPI";
+import News from "../News/News";
+import Crypto from "../Crypto/Crypto";
 
 function Homepage() {
   const { data, isfetching } = useGetCryptoQuery();
-  const globalStat = data?.data?.stats;
+  const globalStats = data?.data?.stats;
 
   console.log(data);
 
@@ -22,21 +24,53 @@ function Homepage() {
       </Typography.Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStat.total} />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total MarketCap" value="5" />
+          <Statistic
+            title="Total MarketCap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24Hr Volume" value="5" />
+          <Statistic
+            title="Total 24Hr Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market" value="5" />
+          <Statistic
+            title="Total Market"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Typography.Title>
+        <Typography.Title level={3} className="show-more">
+          <Link to="/crypto">Show More</Link>
+        </Typography.Title>
+      </div>
+      <Crypto simplified />
+
+      <div className="home-heading-container">
+        <Typography.Title level={2} className="home-title">
+          Latest Crypto News
+        </Typography.Title>
+        <Typography.Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Typography.Title>
+      </div>
+      <News simplified />
     </div>
   );
 }
